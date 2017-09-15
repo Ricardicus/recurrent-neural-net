8be337234c8a9b318c823a6358c6905d93917bda
Rickard Hallerbäck <rickard_hallerback@hotmail.com>
2017-09-15 18:33:59 +0200

    Adding progress viewer

---
+++ view_progres.py
@@ -0,0 +1,25 @@
+import numpy as np
+from matplotlib import pyplot as pl
+import sys
+
+if __name__=="__main__":
+	
+	if len(sys.argv) == 2:
+		name = sys.argv[1]
+
+		with open(name) as f:
+			lines = f.readlines()
+
+			iterations = []
+			losses = []
+
+			for line in lines:
+				try:
+					line.split(',')[1]
+					iterations.append(int(line.split(',')[0]))
+					losses.append(float(line.split(',')[1]))
+				except IndexError:
+					pass
+
+			pl.plot(iterations, losses)
+			pl.show()
