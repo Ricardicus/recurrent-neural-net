30e5d9be76207272c10e5592a66c635b38f900e6
Rickard <rickard.hallerback@gmail.com>
2019-02-14 13:39:41 +0100

    indentation

--- std_conf.h
+++ std_conf.h
@@ -28,29 +28,29 @@
 #ifndef STD_CONF_H
 #define STD_CONF_H
 
-#define NEURONS													128
+#define NEURONS                                                 128
 
-#define STD_LEARNING_RATE										0.001
-#define STD_MOMENTUM											0.0
-#define STD_LAMBDA												0.05
-#define SOFTMAX_TEMP											1.0
-#define GRADIENT_CLIP_LIMIT										5.0
-#define MINI_BATCH_SIZE											100
-#define LOSS_MOVING_AVG											0.01
+#define STD_LEARNING_RATE                                       0.001
+#define STD_MOMENTUM                                            0.0
+#define STD_LAMBDA                                              0.05
+#define SOFTMAX_TEMP                                            1.0
+#define GRADIENT_CLIP_LIMIT                                     5.0
+#define MINI_BATCH_SIZE                                         100
+#define LOSS_MOVING_AVG                                         0.01
 
-#define LAYERS  												3
+#define LAYERS                                                  3 // Has a tremendous impact on avaiable memory
 
-#define STATEFUL												1
+#define STATEFUL                                                1
 
-#define GRADIENTS_CLIP											1
-#define GRADIENTS_FIT											0
+#define GRADIENTS_CLIP                                          1
+#define GRADIENTS_FIT                                           0
 
-#define MODEL_REGULARIZE										0
+#define MODEL_REGULARIZE                                        0
 
-#define DECREASE_LR 											0 // set to 0 to disable decreasing learning rate
+#define DECREASE_LR                                             0 // set to 0 to disable decreasing learning rate
 
-#define STD_LEARNING_RATE_DECREASE								100000
-#define STD_NUMBER_OF_NO_RECORD_ITERATIONS_UNTIL_LR_DECREASE	1000000							
+#define STD_LEARNING_RATE_DECREASE                              100000
+#define STD_NUMBER_OF_NO_RECORD_ITERATIONS_UNTIL_LR_DECREASE    1000000
 
 /*
 * These defines modify how the program interacts with the user
@@ -58,16 +58,16 @@
 * decide how often it should output its progress and wether or not
 * it should write to file among other things.
 */
-#define PRINT_EVERY_X_ITERATIONS								100
-#define STORE_EVERY_X_ITERATIONS								8000
-#define PRINT_PROGRESS 											1   // set to 0 to disable printing
-#define PRINT_SAMPLE_OUTPUT 									1   // set to 0 to disable output sampling
-#define PRINT_SAMPLE_OUTPUT_TO_FILE								0   // set to 0 to disable output sampling to file
-#define PRINT_SAMPLE_OUTPUT_TO_FILE_ARG							"a" // used as an argument to fopen (goes with "w" or "a")
-#define PRINT_SAMPLE_OUTPUT_TO_FILE_NAME						"progress_output.txt" // name of the file containing samples
-#define STORE_PROGRESS_EVERY_X_ITERATIONS						1000 // set to 0 to disable writing loss value to file during training
-#define PROGRESS_FILE_NAME										"progress.csv"
-#define NUMBER_OF_CHARS_TO_DISPLAY_DURING_TRAINING				200
+#define PRINT_EVERY_X_ITERATIONS                                100
+#define STORE_EVERY_X_ITERATIONS                                8000
+#define PRINT_PROGRESS                                          1   // set to 0 to disable printing
+#define PRINT_SAMPLE_OUTPUT                                     1   // set to 0 to disable output sampling
+#define PRINT_SAMPLE_OUTPUT_TO_FILE                             0   // set to 0 to disable output sampling to file
+#define PRINT_SAMPLE_OUTPUT_TO_FILE_ARG                         "a" // used as an argument to fopen (goes with "w" or "a")
+#define PRINT_SAMPLE_OUTPUT_TO_FILE_NAME                        "progress_output.txt" // name of the file containing samples
+#define STORE_PROGRESS_EVERY_X_ITERATIONS                       1000 // set to 0 to disable writing loss value to file during training
+#define PROGRESS_FILE_NAME                                      "progress.csv"
+#define NUMBER_OF_CHARS_TO_DISPLAY_DURING_TRAINING              200
 
 /*
 * Once the network has been trained it is stored to these files.
@@ -77,12 +77,12 @@
 * but not by the interactive HTML application.
 * For that, the .json program is intended to be used.
 */
-#define STD_LOADABLE_NET_NAME									"lstm_net.net"
-#define STD_JSON_NET_NAME										"lstm_net.json"
+#define STD_LOADABLE_NET_NAME                                   "lstm_net.net"
+#define STD_JSON_NET_NAME                                       "lstm_net.json"
 
 // ================== DO NOT CHANGE THE FOLLOWING DEFINES ======================
 // Don't change this one, else the HTML application will not work.
-#define JSON_KEY_NAME_SET										"Feature mapping"
+#define JSON_KEY_NAME_SET                                       "Feature mapping"
 // This define should be undeffed, it was defined during experimentation
 // #define INTERLAYER_SIGMOID_ACTIVATION
 // =============================================================================
