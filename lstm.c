d6f9c02b9e0cd714510ab853fb7a38918a4206b6
Rickard Hallerbäck <rickard_hallerback@hotmail.com>
2019-12-22 19:49:34 +0100

    Small fix

--- lstm.c
+++ lstm.c
@@ -1611,8 +1611,7 @@ void lstm_train(lstm_model_t** model_layers, lstm_model_parameters_t *params,
   set_t* char_index_mapping, unsigned int training_points,
   int* X_train, int* Y_train, unsigned int layers)
 {
-  int status = 0;
-  unsigned int p;
+  int status = 0, p;
   unsigned int i = 0, b = 0, q = 0, e1 = 0, e2 = 0, e3, record_iteration = 0, tmp_count, trailing;
   unsigned long n = 0, epoch = 0;
   double loss = -1, loss_tmp = 0.0, record_keeper = 0.0;
